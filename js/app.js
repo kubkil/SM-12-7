b8d3856bca979e179e0debbcf3d2ad1c2f79f8eb
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-27 19:31:36 +0200

    finish app.js

--- js/app.js
+++ js/app.js
@@ -1,27 +1,33 @@
-// OGÓLNA FUNKCJA
-function randomString() {
-  var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-  var str = '', i;
-  for (i = 0; i < 10; i++) {
-    str += chars[Math.floor(Math.random() * chars.length)];
-  }
-  return str;
+// API
+const baseUrl = 'https://kodilla.com/pl/bootcamp-api';
+const myHeaders = {
+  'X-Client-Id': '3074',
+  'X-Auth-Token': '6c1ba6a5a7c1f79f26f3cb0e0ed81f9e'
 }
 
-// TWORZENIE NOWYCH EGZEMPLARZY KOLUMN
-var todoColumn = new Column('Do zrobienia');
-var doingColumn = new Column('W trakcie');
-var doneColumn = new Column('Skończone');
+$.ajaxSetup({
+  headers: myHeaders
+});
 
-// DODAWANIE KOLUMN DO TABLICY
-board.createColumn(todoColumn);
-board.createColumn(doingColumn);
-board.createColumn(doneColumn);
+$.ajax({
+  url: baseUrl + '/board',
+  method: 'GET',
+  success: function(response) { // co robi tutaj to response?
+    setupColumns(response.columns);
+  }
+});
 
-// TWORZENIE NOWYCH EGZEMPLARZY KART
-var card1 = new Card('Nowe zadanie');
-var card2 = new Card('stworzyc tablice kanban');
+function setupColumns(columns) {
+  columns.forEach(function(column) {
+    const col = new Column(column.id, column.name);
+    board.createColumn(col);
+    setupCards(col, column.cards);
+  });
+}
 
-// DODAWANIE KART DO KOLUMN
-todoColumn.createCard(card1);
-doingColumn.createCard(card2);
+function setupCards(col, cards) {
+  cards.forEach(function(card) {
+    const cardObj = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+    col.createCard(cardObj);
+  })
+}
