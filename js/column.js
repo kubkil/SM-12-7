cc8b339acbdc67848e6ba505c3d5992460b89c9a
Jakub Kilar <kuba.kilar@gmail.com>
2018-03-27 19:32:16 +0200

    finish column.js

--- js/column.js
+++ js/column.js
@@ -1,9 +1,7 @@
-function Column(name) {
-  var self = this;
-
-  this.id = randomString();
-  this.name = name;
-  this.element = createColumn();
+function Column(id, name) { // jakie id i jakie name, skąd się bierze w nawiasach?
+  this.id = id;
+  this.name = name || 'No name given';
+}
 
   function createColumn() {
     // TWORZENIE NOWYCH WĘZŁÓW
@@ -18,9 +16,21 @@ function Column(name) {
       self.deleteColumn();
     });
 
-    columnAddCard.click(function (event) {
+    columnAddCard.click(function(event) {
+      let cardName = prompt('Enter the name of the card')
       event.preventDefault();
-      self.createCard(new Card(prompt("Wpisz nazwę karty")));
+      $.ajax({
+        url: baseUrl + '/card',
+        method: 'POST',
+        data: {
+          name: cardName,
+          bootcamp_kanban_column_id: self.id
+        },
+        success: function(response) {
+          const card = new Card(response.id, cardName);
+          self.createCard(card);
+        }
+      });
     });
 
     // KONSTRUOWANIE ELEMENTU KOLUMNY
@@ -30,12 +40,19 @@ function Column(name) {
       .append(columnCardList);
     return column;
   }
-}
+
 Column.prototype = {
   createCard: function (card) {
     this.element.children('ul').append(card.element);
   },
   deleteColumn: function () {
-    this.element.remove();
+    const self = this;
+    $.ajax({
+      url: baseUrl + '/column/' + self.id,
+      method: 'DELETE',
+      success: function(response) {
+        self.element.remove();
+      }
+    });
   }
 };
